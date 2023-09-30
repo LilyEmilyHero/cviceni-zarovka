3774e589523a06ff453982ba47d3561515111a57
Linda Hrdinová <hrlin@seznam.cz>
2023-09-30 13:55:15 +0200

    Zpracování cvičení na hodině

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log('funguju!');
+/*
+Žárovka
+Ovládejte žárovku.
+Vytvořte si repozitář ze šablony cviceni-zarovka se stránkou, která zobrazuje obyčejnou žárovku.
+
+Pomocí JavaScriptu zařiďte, aby se při stisknutí libovolné klávesy na stránce přidala k elementu žárovky CSS třída bulb--on. Žárovka by se takto měla rozsvítit.
+Vylepšete program tak, aby na následné stisknutí libovolné klávesy žárovka opět zhasnula. Opakovaným mačkáním kláves ji tak můžeme rozsvěcovat a zhasínat.
+*/
+
+const bulbElement = document.querySelector('.bulb');
+
+const turnBulb = () => {
+  if (bulbElement.classList.contains('bulb--on')) {
+    bulbElement.classList.remove('bulb--on');
+  } else {
+    bulbElement.classList.add('bulb--on');
+  }
+};
+
+document.addEventListener('keydown', turnBulb);
+
+/*
+bulbElement.classList.add('bulb--on');
+*/
